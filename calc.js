48c593d69b69e39ae5689bd543d72d9a43b5aede
akshay <akshayantovadakkan@gmail.com>
2023-05-11 13:21:41 +0530

    initial commit

---
+++ calc.js
@@ -0,0 +1,15 @@
+function displayData(n){
+    result.value+=n
+}
+function evalOp(){
+    result.value=eval(result.value)
+
+}
+
+function AC(){
+    result.value=""
+}
+
+function bacSp(){
+    result.value=result.value.slice(0,-1)
+}
